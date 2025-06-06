b05d7dae14a372ec1b90d33b876a2a541c90f7b3
dmorocode <danilomoro@hotmail.com>
2025-06-05 22:56:38 -0300

    Corrige importação em wsgi.py para usar app_backend.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,2 @@
 # wsgi.py
-from src.app_backend import app as application # 'app' é a instância do Flask no seu app.py
-# Se seu app.py estiver dentro de uma subpasta, tipo 'src/app.py',
-# seria 'from src.app import app as application'
+from app_backend import app as application
